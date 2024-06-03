7f8b0b605dfa4cbf7a941e3efd1e3e472e420e9e
yathriajkr <144806369+yathriajkr@users.noreply.github.com>
2024-06-03 14:41:16 +0530

    Update __init__.py

--- web_app/board/__init__.py
+++ web_app/board/__init__.py
@@ -10,10 +10,10 @@ def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
 
-    database.init_app(app)
+    #database.init_app(app)
     
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
     print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
-    print(f"Using Database: {app.config.get('DATABASE')}")
+    #print(f"Using Database: {app.config.get('DATABASE')}")
     return app
