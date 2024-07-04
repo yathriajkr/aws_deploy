6163aff10872da53aa9a8c28ded6fff943d343d0
yathriajkr <144806369+yathriajkr@users.noreply.github.com>
2024-07-04 23:53:21 +0530

    Update standard.py

--- firewall/filter_plugins/standard.py
+++ firewall/filter_plugins/standard.py
@@ -1,13 +1,19 @@
+def fetch_static_info(device_data):
+  for data in device_data:
+
+  return trim_data
 
 def validate(std_config, device_config):
   print("Hello")
-  some = "Hello "+ hostname
-  return some
+  
+  miss_routes = "Hello "+ hostname
+  return miss_routes
 
 
 class FilterModule(object):
     def filters(self):
         return {
-            'validate': validate
+            'validate_static': validate,
+            'filter_static': fetch_static_info
         }
 
