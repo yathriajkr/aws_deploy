52f211bd84c0b7b5326cc514ea3e0769f31aa6ba
yathriajkr <144806369+yathriajkr@users.noreply.github.com>
2024-07-05 03:38:27 +0530

    Update standard.py

--- firewall/filter_plugins/standard.py
+++ firewall/filter_plugins/standard.py
@@ -1,24 +1,23 @@
 def ftr_zone(device_data):
-  print(device_data)
+  # print(device_data)
   zones = device_data["ansible_facts"]["ansible_net_virtual-systems"][0]["vsys_zonelist"]
   #for data in device_data:
   return zones
 
 def validate_zone(std_config, device_config):
-  print(device_config)
-  print("Hello")
+  # print(device_config)
   missing_zones = []
   temp = {}
   for zone in std_config:
-    print("Enter zone")
+    # print("Enter zone")
     print(zone)
     temp = {}
     if zone["zone_name"] not in device_config:
-      print("found match")
+      # print("found match")
       temp = zone
       missing_zones.append(temp)
     
-  print(missing_zones)
+  # print(missing_zones)
   return missing_zones
 
 def xml2json(value):
