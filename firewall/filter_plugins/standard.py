26d7ca67da8508c7510df8c974c5d183c6b22813
yathriajkr <144806369+yathriajkr@users.noreply.github.com>
2024-07-05 03:12:02 +0530

    Update standard.py

--- firewall/filter_plugins/standard.py
+++ firewall/filter_plugins/standard.py
@@ -5,13 +5,16 @@ def ftr_zone(device_data):
   return zones
 
 def validate_zone(std_config, device_config):
+  print(device_config)
   print("Hello")
   missing_zones = []
   temp = {}
   for zone in std_config:
+    print("Enter zone")
     print(zone)
     temp = {}
     if zone["zone_name"] not in device_config:
+      print("found match")
       temp = zone
     missing_zones.append(temp)
     
