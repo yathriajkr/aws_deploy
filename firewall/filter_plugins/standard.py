7af4cd1fb85230166690f1d2a9f8f4823858fca9
yathriajkr <144806369+yathriajkr@users.noreply.github.com>
2024-07-05 01:02:00 +0530

    Update standard.py

--- firewall/filter_plugins/standard.py
+++ firewall/filter_plugins/standard.py
@@ -1,23 +1,25 @@
-def ftr_static_route(device_data):
+def ftr_zone(device_data):
   print(device_data)
   zones = device_data["ansible_facts"]["ansible_net_virtual-systems"][0]["vsys_zonelist"])
   #for data in device_data:
-  
-  return device_data
+  return zones
 
-def validate_zones(std_config, device_config):
+def validate_zone(std_config, device_config):
   print("Hello")
-  missing = {}
-  temp
-  if std_config not in device_config
-  
-  miss_routes = "Hello "
-  return miss_routes
-
+  missing_zones = []
+  temp = {}
+  for zone in std_config:
+    temp = {}
+    if zone.zone_name not in device_config:
+      temp = zone
+    missing_zones.append(temp)
+    
+  print(missing_zones)
+  return missing_zones
 
 class FilterModule(object):
     def filters(self):
         return {
-            'ftr_static': ftr_static_route,
-            'validate_static': validate_static
+            'ftr_zone': ftr_zone,
+            'validate_zone': validate_zone
         }
