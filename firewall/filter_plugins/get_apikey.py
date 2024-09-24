319f5a7222e56f0531d17b896ee7e9904f90f9e9
yathriajkr <144806369+yathriajkr@users.noreply.github.com>
2024-09-24 11:13:30 +0530

    Update get_apikey.py

--- firewall/filter_plugins/get_apikey.py
+++ firewall/filter_plugins/get_apikey.py
@@ -1,1 +1,14 @@
 
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'xml2json': xml2json,
+            'ftr_zone': ftr_zone,
+            'validate_zone': validate_zone,
+            'ftr_addrobj': ftr_addrobj,
+            'validate_addrobj': validate_addrobj,
+            'ftr_servobj': ftr_servobj,
+            'validate_servobj': validate_servobj
+        }
